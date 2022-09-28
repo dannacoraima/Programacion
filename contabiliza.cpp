405d6ba0468462ed34c135a13d6630a493853b34
dannacoraima <danna.santillan.yela@utelvt.edu.ec>
2022-09-28 17:27:29 -0500

    version2

--- contabiliza.cpp
+++ contabiliza.cpp
@@ -3,21 +3,25 @@ using namespace std;
 
 int main()
 {
-	int i=0,l;
-	float x,s=0;
-	cout<<"ingrese el limite l="; cin>>l;
+	int DCSY_i=0,DCSY_l;
+	float DCSY_x,DCSY_s=0;
+	cout<<"ingrese el limite DCSY_l="; cin>>DCSY_l;
 	do{
 		
-	cout<<"ingrese el numero x="; cin>>x;
-	i=i+1;
-	s=s+x;
+	cout<<"ingrese el numero DCSY_x="; cin>>DCSY_x;
+	DCSY_i=DCSY_i+1;
+	DCSY_s=DCSY_s+DCSY_x;
 
 
 
-	}while(i<l);
-	cout<<"Se ingresaron "<<l<<" numeros "<< "que sumaron "<<s<<endl;
+	}while(DCSY_i<DCSY_l);
+	cout<<"Se ingresaron "<<DCSY_l<<" numeros "<< "que sumaron "<<DCSY_s<<endl;
 	return 0;
 
 
 
 }
+
+
+
+
