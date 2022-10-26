88fa8aaefaa24d0aaa30e6479e452ed24cbc6496
dannacoraima <111997705+dannacoraima@users.noreply.github.com>
2022-10-25 20:24:08 -0500

    Create Mayoralpromedio.cpp

---
+++ Mayoralpromedio.cpp
@@ -0,0 +1,28 @@
+ //programa para que calcula para saber el mayor promedio
+            //nombre: Santillan Danna
+//Curso :Tics "B"
+        //docente:Ing.Stalin Francis
+
+#include<iostream>
+using namespace std;
+int main()
+
+{
+	float x,pm=0;
+	int i=0,l;
+	cout<<"ingrese l: "; cin>>l;
+	do{
+
+	cout<<"ingrese x:  "; cin>>x;
+	i=i+1;
+	if(x>pm){
+
+		pm=x;
+
+	}
+
+	}while(i<l);
+	cout<<"el promedio maximo del curso fue:  "<<pm<<endl<<endl;
+	return 0;
+
+}
